2e552359e28930befa0609958ce309626e340089
Jimmy Howe <jimmyhowe@live.co.uk>
2015-12-08 21:23:02 +0000

    Update casts to object

--- app/Product.php
+++ app/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $casts = [
-        'prices' => 'json',
-        'options' => 'json'
+        'prices' => 'object',
+        'options' => 'object'
     ];
 }
