8de0233c8cdb01ab06961061879b230a059b0458
Jimmy Howe <jimmyhowe@live.co.uk>
2015-12-10 22:03:42 +0000

    Add vue and run gulp

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,9 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
+
     mix.sass('app.scss');
+
+    mix.browserify('app.js');
+
 });
---
+++ resources/assets/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Jimmy on 10/12/2015.
+ */
+
+var Vue = require('vue');
+
+Vue.use(require('vue'));
+
+new Vue({
+    
+});
