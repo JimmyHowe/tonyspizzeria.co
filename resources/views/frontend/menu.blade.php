da19a73e0af22a13d723e23bc014e11954166dbf
Jimmy Howe <jimmyhowe@live.co.uk>
2015-12-15 06:59:35 +0000

    Add jump list

--- resources/views/frontend/menu.blade.php
+++ resources/views/frontend/menu.blade.php
@@ -7,13 +7,7 @@
         <div class="col-md-4">
 
             <div class="list-group">
-                <a href="#" class="list-group-item active">
-                    Cras justo odio
-                </a>
-                <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
-                <a href="#" class="list-group-item">Morbi leo risus</a>
-                <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-                <a href="#" class="list-group-item">Vestibulum at eros</a>
+                <a v-for="group in menu.groups" href="#@{{ group.slug }}" class="list-group-item">ITEM</a>
             </div>
 
         </div>
